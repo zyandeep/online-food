4edb1b4901e25470ed83bbac0babe68b8674c58b
unknown <ravisingha@gmail.com>
2021-05-28 17:37:53 +0530

    admin dash

---
+++ application/controllers/admin_dashboard.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class admin_dashboard extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database(); 
+        
+    }
+
+    public function index()
+    {
+        $this->load->view('includes/header');
+        $this->load->view('admin_Dashboard');
+       
+       // $this->load->view('includes/footer');
+    }
+
+ 
+
+}
